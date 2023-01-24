285b256fb1bee17217c88169e79b562424bc2816
Kevin <kevin.thai@edhec.com>
2023-01-24 09:22:02 +0100

    lab 14 please

---
+++ Lab14/lab14.sql
@@ -0,0 +1,82 @@
+select * from olist.order_items;
+
+-- 1. From the order_items table, find the price of the highest priced order and lowest price order.
+SELECT
+MAX(price),
+MIN(price)
+FROM olist.order_items
+;
+
+-- 2. From the order_items table, what is range of the shipping_limit_date of the orders?
+SELECT
+MIN(shipping_limit_date),
+MAX(shipping_limit_date)
+FROM olist.order_items
+;
+
+SELECT * FROM olist.customers;
+
+-- 3. From the customers table, find the states with the greatest number of customers.
+SELECT 
+customer_state,
+COUNT(DISTINCT customer_id),
+COUNT(DISTINCT customer_unique_id)
+FROM olist.customers
+GROUP BY customer_state
+ORDER BY COUNT(DISTINCT customer_id) DESC
+LIMIT 1
+;
+
+-- 4. From the customers table, within the state with the greatest number of customers, find the cities with the greatest number of customers.
+SELECT 
+customer_city,
+COUNT(DISTINCT customer_id),
+COUNT(DISTINCT customer_unique_id)
+FROM olist.customers
+WHERE customer_state = 'SP'
+GROUP BY customer_city
+ORDER BY COUNT(DISTINCT customer_id) DESC
+;
+
+SELECT * from olist.closed_deals;
+-- 5. From the closed_deals table, how many distinct business segments are there (not including null)?
+SELECT COUNT(DISTINCT business_segment)
+FROM olist.closed_deals
+WHERE business_segment IS NOT NULL;
+
+-- 6. From the closed_deals table, sum the declared_monthly_revenue for duplicate row values in business_segment 
+-- and find the 3 business segments with the highest declared monthly revenue (of those that declared revenue).
+SELECT
+business_segment,
+SUM(declared_monthly_revenue)
+FROM olist.closed_deals
+WHERE business_segment IS NULL
+GROUP BY
+business_segment
+;
+
+SELECT
+business_segment,
+SUM(declared_monthly_revenue)
+FROM olist.closed_deals
+GROUP BY
+business_segment
+ORDER BY SUM(declared_monthly_revenue) DESC
+LIMIT 3
+;
+
+SELECT * FROM olist.order_reviews;
+-- 7. From the order_reviews table, find the total number of distinct review score values.
+SELECT DISTINCT review_score
+FROM olist.order_reviews;
+
+-- 8. In the order_reviews table, create a new column with a description that corresponds 
+-- to each number category for each review score from 1 - 5, then find the review score and category occurring most frequently in the table.
+
+-- 9. From the order_reviews table, find the review value occurring most frequently and how many times it occurs.
+SELECT
+review_score,
+COUNT(DISTINCT review_id)
+FROM olist.order_reviews
+GROUP BY review_score
+ORDER BY COUNT(DISTINCT review_id) DESC;
---
+++ Lab14/lab14answers.md
@@ -0,0 +1,33 @@
+1. From the order_items table, find the price of the highest priced order and lowest price order.
+
+Max: 6735	
+Min: 0.85
+
+2. From the order_items table, what is range of the shipping_limit_date of the orders?
+
+[2016-09-19 02:15:34 , 2020-04-10 00:35:08]
+
+3. From the customers table, find the states with the greatest number of customers.
+SP
+
+4. From the customers table, within the state with the greatest number of customers, find the cities with the greatest number of customers.
+sao paulo	15540	
+
+5. From the closed_deals table, how many distinct business segments are there (not including null)?
+33
+
+6. From the closed_deals table, sum the declared_monthly_revenue for duplicate row values in business_segment and find the 3 business segments with the highest declared monthly revenue (of those that declared revenue).
+0
+and
+construction_tools_house_garden	50695006
+phone_mobile	8000000
+home_decor	710000
+
+7. From the order_reviews table, find the total number of distinct review score values.
+5
+
+8. In the order_reviews table, create a new column with a description that corresponds to each number category for each review score from 1 - 5, then find the review score and category occurring most frequently in the table.
+I don't understand the question.
+
+9. From the order_reviews table, find the review value occurring most frequently and how many times it occurs.
+5	57000
